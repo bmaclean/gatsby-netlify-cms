d76d4def014a221ec0d97f38290e09b78b8c9202
Brendan M <brendan.maclean94@gmail.com>
2019-07-27 13:39:00 -0600

    Remove admin page references and include only Contact and Home pages.

--- src/cms/preview-templates/ContactPagePreview.js
+++ src/cms/preview-templates/ContactPagePreview.js
@@ -2,18 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { ContactPageTemplate } from '../../templates/contact-page'
 
-const AboutPagePreview = ({ entry, widgetFor }) => (
+const ContactPagePreview = ({ entry, widgetFor }) => (
   <ContactPageTemplate
     title={entry.getIn(['data', 'title'])}
     content={widgetFor('body')}
   />
 )
 
-AboutPagePreview.propTypes = {
+ContactPagePreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
   widgetFor: PropTypes.func,
 }
 
-export default AboutPagePreview
+export default ContactPagePreview
